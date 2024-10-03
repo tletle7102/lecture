0ddc1ed0f06fa1137915aa634f3e02c0c4398ded
tletle7102 <tletle7102@gmail.com>
2024-10-03 18:54:11 +0900

    feat(git add): second springboot lecture commit

---
+++ src/test/java/com/springboot/lecture/LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
