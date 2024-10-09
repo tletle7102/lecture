00ebfb85e5d11d5a80fde7206d983d5e5e1358d7
tletle7102 <tletle7102@gmail.com>
2024-10-09 15:47:13 +0900

    feat(btr issue): temporary git commit issue occured

---
+++ src/main/java/com/springboot/lecture/controller/GetController.java
@@ -0,0 +1,23 @@
+package com.springboot.lecture.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/get-api")
+public class GetController {
+
+    // http://localhost:8080/get-api/call-cafe
+    @RequestMapping(value = "/call-cafe", method = RequestMethod.GET)
+    public String coffee(){
+        return "커피나왔어요!!!";
+    }
+
+    // http://localhost:8080/get-api/korea-love
+    @RequestMapping(value = "/korea-love", method = RequestMethod.GET)
+    public String tea(){
+        return "홍차나왔어요!!!";
+    }
+
+}
