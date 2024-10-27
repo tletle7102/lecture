56c00bb8a076a0972554eaaeb4cb44aba8aa733f
tletle7102 <tletle7102@gmail.com>
2024-10-27 17:29:06 +0900

    feat(DAO): product DAO and DAOImpl 생성

---
+++ src/main/java/com/springboot/lecture/data/dao/ProductDAO.java
@@ -0,0 +1,12 @@
+package com.springboot.lecture.data.dao;
+
+import com.springboot.lecture.data.entity.Product;
+
+public interface ProductDAO {
+
+    Product insertProduct(Product product);
+    Product selectProduct(Long number);
+    Product updateProductName(Long number, String name) throws Exception;
+    void deleteProduct (Long number) throws Exception;
+
+}
