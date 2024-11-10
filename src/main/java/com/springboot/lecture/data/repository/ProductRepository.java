f7a31d4f78b24c03709e0f8546dcba3c8ae8f930
tletle7102 <tletle7102@gmail.com>
2024-11-10 20:00:04 +0900

    chore(repo): 코드에 대한 주석 추가

--- src/main/java/com/springboot/lecture/data/repository/ProductRepository.java
+++ src/main/java/com/springboot/lecture/data/repository/ProductRepository.java
@@ -3,8 +3,7 @@ package com.springboot.lecture.data.repository;
 import com.springboot.lecture.data.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// DAO 또는 서비스에서 DI(의존성주입)해서 사용하기 위해서 리포지토리를 가장 먼저 문서화해야함(일빠)
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-
 
 }
