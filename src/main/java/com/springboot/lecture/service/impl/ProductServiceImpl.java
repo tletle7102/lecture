c2317ae9f8413cec6c5ea25c71bd3b954f25e38a
tletle7102 <tletle7102@gmail.com>
2024-11-16 18:26:55 +0900

    feat(product): save, change, delete method 구현

--- src/main/java/com/springboot/lecture/service/impl/ProductServiceImpl.java
+++ src/main/java/com/springboot/lecture/service/impl/ProductServiceImpl.java
@@ -8,6 +8,8 @@ import com.springboot.lecture.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 // 커피머신
 public class ProductServiceImpl implements ProductService {
@@ -51,16 +53,44 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductResponseDto saveProduct(ProductDto productDto) {
-        return null;
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setPrice(productDto.getPrice());
+        product.setStock(productDto.getStock());
+        product.setCreatedAt(LocalDateTime.now());
+        product.setUpdatedAt(LocalDateTime.now());
+
+        Product savedProduct = productDAO.insertProduct(product);
+
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+
+        productResponseDto.setNumber(savedProduct.getNumber());
+        productResponseDto.setName(savedProduct.getName());
+        productResponseDto.setPrice(savedProduct.getPrice());
+        productResponseDto.setStock(savedProduct.getStock());
+
+        return productResponseDto;
     }
 
     @Override
     public ProductResponseDto changeProductName(Long number, String name) throws Exception {
-        return null;
+
+        Product changeProduct = productDAO.updateProductName(number, name);
+
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setNumber(changeProduct.getNumber());
+        productResponseDto.setName(changeProduct.getName());
+        productResponseDto.setPrice(changeProduct.getPrice());
+        productResponseDto.setStock(changeProduct.getStock());
+
+        return productResponseDto;
     }
 
     @Override
     public void deleteProduct(Long number) throws Exception {
+
+        productDAO.deleteProduct(number);
 
     }
 }
