1255044e6fb8ed09a86a98c35d8c4749b79254df
tletle7102 <tletle7102@gmail.com>
2024-11-17 19:41:21 +0900

    feat(product): TDD를 하기 위한 test를 위해 기존의 DAO를 Repository로 변경, 내장되어있는 Logger를 DI하여 사용하도록 변경

--- src/main/java/com/springboot/lecture/service/impl/ProductServiceImpl.java
+++ src/main/java/com/springboot/lecture/service/impl/ProductServiceImpl.java
@@ -4,7 +4,10 @@ import com.springboot.lecture.data.dao.ProductDAO;
 import com.springboot.lecture.data.dto.ProductDto;
 import com.springboot.lecture.data.dto.ProductResponseDto;
 import com.springboot.lecture.data.entity.Product;
+import com.springboot.lecture.data.repository.ProductRepository;
 import com.springboot.lecture.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,22 +17,24 @@ import java.time.LocalDateTime;
 // 커피머신
 public class ProductServiceImpl implements ProductService {
 
-    //  ProductServiceImpl을 사용하기 위해 밑에 DAO부분을 먼저 선언해야함(먼저 존재해야만하니까, 커피캡슐)
-    private final ProductDAO productDAO;
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductServiceImpl.class);
+    private final ProductRepository productRepository;
 
 
     //  생성자로 필드(ProductDAO) 초기화 == 의존성주입
-    public ProductServiceImpl(ProductDAO productDAO) {
-        this.productDAO = productDAO;
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     // 자동완성으로 오버라이딩 메서드 설정하기
     @Override
     public ProductResponseDto getProduct(Long number) {
 
-        // 선언하고 생성자로 초기화한(DI한) productDAO를 이용해서 number(parameter)로
-        // Product타입의 데이터를 조회해서 product라는 변수에 할당함
-        Product product = productDAO.selectProduct(number);
+        LOGGER.info("[getProduct] input number : {}", number);
+        Product product =productRepository.findById(number).get();
+
+        LOGGER.info("[getProduct] product number : {} , name : {}", product.getNumber(),
+                product.getName());
 
         // getProduct메서드의 반환타입이 ProductResponseDto이므로,
         // ProductResponseDto 타입의 인스턴스를 생성하여,
@@ -54,14 +59,16 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductResponseDto saveProduct(ProductDto productDto) {
 
+        LOGGER.info("[saveProduct] productDTO : {}", productDto.toString());
+
         Product product = new Product();
         product.setName(productDto.getName());
         product.setPrice(productDto.getPrice());
         product.setStock(productDto.getStock());
-        product.setCreatedAt(LocalDateTime.now());
-        product.setUpdatedAt(LocalDateTime.now());
 
-        Product savedProduct = productDAO.insertProduct(product);
+
+        Product savedProduct = productRepository.save(product);
+        LOGGER.info("[saveProduct] savedProduct : {}", savedProduct);
 
         ProductResponseDto productResponseDto = new ProductResponseDto();
 
@@ -76,7 +83,9 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public ProductResponseDto changeProductName(Long number, String name) throws Exception {
 
-        Product changeProduct = productDAO.updateProductName(number, name);
+        Product foundProduct = productRepository.findById(number).get();
+        foundProduct.setName(name);
+        Product changeProduct =  productRepository.save(foundProduct);
 
         ProductResponseDto productResponseDto = new ProductResponseDto();
         productResponseDto.setNumber(changeProduct.getNumber());
@@ -90,7 +99,7 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void deleteProduct(Long number) throws Exception {
 
-        productDAO.deleteProduct(number);
+        productRepository.deleteById(number);
 
     }
 }
