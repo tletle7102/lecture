543d6fbeb3278338e0707e012c91d72c03ea8f15
tletle7102 <tletle7102@gmail.com>
2025-01-21 18:51:53 +0900

    feat(Validated): ValidatedRequestDto 생성

---
+++ src/main/java/com/springboot/lecture/data/dto/ValidatedRequestDto.java
@@ -0,0 +1,37 @@
+package com.springboot.lecture.data.dto;
+
+import com.springboot.lecture.configuration.annotation.Telephone;
+import com.springboot.lecture.data.group.ValidationGroup1;
+import com.springboot.lecture.data.group.ValidationGroup2;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class ValidatedRequestDto {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    private String email;
+
+    @Telephone
+    private String phoneNumber;
+
+    @Min(value = 20, groups = ValidationGroup1.class)
+    @Max(value = 40, groups = ValidationGroup2.class)
+    private int age;
+
+    @Size(min = 0, max = 40)
+    private String description;
+
+    @Positive(groups = ValidationGroup2.class)
+    private int count;
+
+    @AssertTrue
+    private boolean booleanCheck;
+}
