9166f2f3b5fd4a0cf45720cae909afc4e1c8e833
tletle7102 <tletle7102@gmail.com>
2025-01-29 17:42:04 +0900

    feat(Constants): 커스텀예외의 출처를 명확하고 체계적으로 관리하기 위해서 도메인 정보를 미리 열거형으로 생성해놓는 Constants

---
+++ src/main/java/com/springboot/lecture/common/Constants.java
@@ -0,0 +1,25 @@
+package com.springboot.lecture.common;
+
+public class Constants {
+
+    public enum ExceptionClass{
+
+        PRODUCT("product");
+
+        private String exceptionClass;
+
+        ExceptionClass(String exceptionClass) {
+            this.exceptionClass =exceptionClass;
+        }
+
+        public String getExceptionClass(){
+            return exceptionClass;
+        }
+
+        @Override
+        public String toString() {
+            return getExceptionClass() + " Exception. ";
+        }
+
+    }
+}
