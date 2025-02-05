7d0be756761e4a4914e10852316639b602d7e5c8
tletle7102 <tletle7102@gmail.com>
2025-02-05 11:19:21 +0900

    feat(Security): 접근 거부 시 예외 처리를 위한 CustomAccessDeniedHandler 구현

--- src/main/java/com/springboot/lecture/configuration/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/springboot/lecture/configuration/security/CustomAccessDeniedHandler.java
@@ -1,6 +1,5 @@
 package com.springboot.lecture.configuration.security;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
