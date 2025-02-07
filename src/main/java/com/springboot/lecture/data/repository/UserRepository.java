98d4f414388391438e60c55752b9ec28d8bb2fd2
tletle7102 <tletle7102@gmail.com>
2025-02-08 07:05:44 +0900

    feat(UserRepository): 사용자 조회 기능 추가 - findByUid(String uid): Optional을 사용하여 UID 기반 사용자 조회 - getByUid(String username): UserDetails 형태로 사용자 조회하여 Spring Security 인증에 활용 - JpaRepository<User, Long> 확장하여 기본적인 CRUD 기능 지원

--- src/main/java/com/springboot/lecture/data/repository/UserRepository.java
+++ src/main/java/com/springboot/lecture/data/repository/UserRepository.java
@@ -2,8 +2,13 @@ package com.springboot.lecture.data.repository;
 
 import com.springboot.lecture.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User getByUid(String uid);
+    Optional<User> findByUid(String uid); // Optional 사용
+
+    UserDetails getByUid(String username);
 }
