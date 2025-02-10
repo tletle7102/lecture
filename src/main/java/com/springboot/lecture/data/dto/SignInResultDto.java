53716fcf90fe9928f9d4ef3785e03bdf8b8f5413
tletle7102 <tletle7102@gmail.com>
2025-02-10 17:59:12 +0900

    feat(Auth): 로그인 응답 DTO 추가

---
+++ src/main/java/com/springboot/lecture/data/dto/SignInResultDto.java
@@ -0,0 +1,23 @@
+package com.springboot.lecture.data.dto;
+
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class SignInResultDto extends SignUpResultDto{
+
+    private String token;
+
+    @Builder
+    public SignInResultDto(boolean success,
+                           int code,
+                           String msg,
+                           String token) {
+
+        super(success,code,msg);
+        this.token = token;
+    }
+}
