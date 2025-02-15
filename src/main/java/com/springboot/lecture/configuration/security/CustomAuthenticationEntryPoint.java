996f377d9a487c3297ad537b5acff7920b79d0fb
tletle7102 <tletle7102@gmail.com>
2025-02-15 16:41:48 +0900

    feat(auth): CustomAuthenticationEntryPoint 클래스에 컴포넌트 어노테이션 추가
    - 스프링부트로 하여금, bean으로 등록하게끔 설정

--- src/main/java/com/springboot/lecture/configuration/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/springboot/lecture/configuration/security/CustomAuthenticationEntryPoint.java
@@ -9,15 +9,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     private final Logger LOGGER = LoggerFactory.getLogger(CustomAuthenticationEntryPoint.class);
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
 
         ObjectMapper objectMapper = new ObjectMapper();
         LOGGER.info("[commence] 인증 실패로 response.sendError 발생");
